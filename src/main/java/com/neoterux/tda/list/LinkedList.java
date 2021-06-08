d6eb2459f61f0b24cd3e9cd9ec52de690ffe5e8d
neoterux <labfernandez2014@gmail.com>
2021-06-08 09:33:29 -0500

    new implementation of LinkedList with double linked node

--- src/main/java/com/neoterux/tda/list/LinkedList.java
+++ src/main/java/com/neoterux/tda/list/LinkedList.java
@@ -1,219 +1,116 @@
 package com.neoterux.tda.list;
 
-import java.util.Objects;
-
-/**
- * Implementación de una LinkedList simple no circular.
- *
- * @param <E> tipo de dato que almacena la lista
- */
 public class LinkedList<E> implements List<E>{
 
-    /**
-     * Primer objeto en la lista.
-     */
-    private NodeSimple<E> header;
-    /**
-     * Último objeto en la lista.
-     */
-    private NodeSimple<E> last;
-    /**
-     * Se utiliza para no tener que recorrer la lista en caso de querer obtener el tamaño.
-     */
-    private int effectiveSize = 0;
+    private Node<E> first;
 
-    /**
-     * Crea un nuevo LinkedList vacío.
-     */
-    public LinkedList() { }
+    private Node<E> last;
 
+    private int effectiveSize;
 
-    /**
-     * Añade un nuevo elemento al inicio de la lista tiene una dificultad de O(1).
-     * @param e nuevo elemento a añadir.
-     * @return true si se añadió con éxito, caso contrario false.
-     */
+    public LinkedList() { effectiveSize = 0; }
+
     @Override
     public boolean addFirst(E e) {
-        NodeSimple<E> nfirst = new NodeSimple<>(e);
-        if(header != null)
-            nfirst.setNext(header);
-        else
-            last = nfirst;
-
-        header = nfirst;
-        effectiveSize++;
-        return true;
+        return false;
     }
 
-    /**
-     * Añade un nuevo elemento al  final de la lista, tiene una dificultad de O(1).
-     *
-     * @param e elemento a añadir.
-     * @return true si se añadió con éxito.
-     */
     @Override
     public boolean addLast(E e) {
-        var nNode = new NodeSimple<>(e);
-        if (header == null) {
-            header = nNode;
-        }else {
-            last.setNext(nNode);
-        }
-        last = nNode;
-        effectiveSize++;
-    return true;
+        return false;
     }
 
-    /**
-     * Añade un elemento a un índice específico. En el peor caso tiene dificultad O(n)
-     *
-     * @param index indice a añadir
-     * @param element elemento a añadir
-     */
     @Override
     public void add(int index, E element) {
-        Objects.checkIndex(index, effectiveSize);
-        NodeSimple<E> nNode = new NodeSimple<>(element);
-        if(index == 0) {
-            nNode.setNext(header);
-            header = nNode;
-        }else {
-            NodeSimple<E> tmp = getNodeAt(index-1);
-            nNode.setNext(tmp.getNext());
-            tmp.setNext(nNode);
-        }
-        effectiveSize++;
+
     }
 
-    /**
-     * Borra un nodo en la lista. En el peor caso su dificultad es de O(n).
-     * @param index indice del elemento a añadir
-     * @return Elemento a borrar de la lista.
-     */
     @Override
     public E remove(int index) {
-        Objects.checkIndex(index, effectiveSize);
-        E removed;
-        if(index == 0 && header != null) {
-            removed = header.getContent();
-            header = header.getNext();
-        }else {
-            NodeSimple<E> tmp = getNodeAt(index - 1);
-            removed = tmp.getNext().getContent();
-            tmp.setNext(null);
-            if(index == effectiveSize -1)
-                last = tmp;
-        }
-        effectiveSize--;
-        return removed;
+        return null;
     }
 
-    /**
-     * Obtiene un objeto en el indice especificado. Tiene una dificultad en el peor de
-     * los casos de O(n).
-     *
-     * @param index indice del objeto a tener.
-     * @return El objeto en el índice especificado
-     */
     @Override
     public E get(int index) {
-        if (header == null)
-            return null;
-        if (index == 0){
-            return header.getContent();
-        }else if (index == effectiveSize -1){
-            return last.getContent();
-        }else {
-            return getNodeAt(index).getContent();
-        }
+        return null;
     }
 
-    /**
-     * Reemplaza y coloca un nuevo objeto en el indice especificado.
-     *
-     * @param index indice a reemplazar
-     * @param element objeto a colocar
-     * @return el objeto que se encontraba en el indice especificado.
-     */
     @Override
     public E set(int index, E element) {
-        Objects.checkIndex(index, effectiveSize);
-        var nNode = new NodeSimple<>(element);
-        E detached = null;
-        if(index == 0){
-            nNode.setNext(header.getNext());
-            detached = header.getContent();
-            header = nNode;
-        }else if (index == effectiveSize -1){
-            last.setNext(nNode);
-            last = nNode;
-        }else {
-            var node = getNodeAt(index-1);
-            detached = node.getNext().getContent();
-            nNode.setNext(node.getNext().getNext());
-            node.setNext(nNode);
-        }
-        return detached;
+        return null;
     }
 
-    /**
-     * @return El tamaño de la Lista.
-     */
     @Override
     public int size() {
-        return effectiveSize;
+        return 0;
     }
 
-    /**
-     * @return true si la lista esta vacía.
-     */
     @Override
     public boolean isEmpty() {
-        return header == null && last == null;
+        return false;
     }
 
-    /**
-     * Borra todos los elementos de la lista
-     */
     @Override
     public void clear() {
-        // Si borramos la referencia al Header y al last
-        // pasan a ser candidatos para el GC, debido a que los objetos de
-        // tipo nodo son package-private. y nadie más tiene referencia a estos.
-        header = null;
-        last = null;
-        // System.gc();
+
     }
 
-    private NodeSimple<E> getNodeAt(int index) {
-        Objects.checkIndex(index, effectiveSize);
-        if(index == 0){
-            return header;
-        }else if (index == effectiveSize -1){
-            return last;
-        }
-        NodeSimple<E> tmp = header;
-        for (int i = 0; i < index; i++) {
-            tmp = tmp.getNext();
-        }
-        return tmp;
-    }
 
-    /**
-     * @return La representacion de la lista en formato [e1, e2, ..., en].
-     */
-    @Override
-    public String toString() {
-        StringBuilder str = new StringBuilder("[");
-        var tmp = header;
-        for (int i = 0; i < effectiveSize; i++) {
-            str.append(tmp.getContent());
-            if (i != effectiveSize -1)
-                str.append(", ");
-            tmp = tmp.getNext();
+    private static class Node<E> {
+        Node<E> previus;
+        E content;
+        Node<E> next;
+
+        Node(E element) {
+            this(element, null, null);
         }
-        str.append("]");
-        return str.toString();
+        Node(E element, Node<E> previus) {
+            this(element, previus, null);
+        }
+        Node(E element, Node<E> previus, Node<E> next){
+            this.content = element;
+            this.previus = previus;
+            this.next = next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public void setPrevius(Node<E> previus) {
+            this.previus = previus;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public E getContent() {
+            return content;
+        }
+
+        public Node<E> getPrevius() {
+            return previus;
+        }
+
+        public Node<E> genNext(final E element) {
+            return new Node<>(element, this);
+        }
+
+        public Node<E> genPrevius(final E element) {
+            Node<E> nNode = new Node<>(element, this.previus, this);
+            var prev = nNode.previus;
+            if(prev != null){
+                // clear previus node reference to this, and replace with new node
+                prev.next = nNode;
+            }
+            // now the previus node of this node is the new node
+            this.previus = nNode;
+            return nNode;
+        }
+        public void clearReferences(){
+            this.next = null;
+            this.previus = null;
+        }
     }
 }
