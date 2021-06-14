34b538c7c72e347fb5624cb9b70f0b9c998f6c4f
neoterux <labfernandez2014@gmail.com>
2021-06-14 17:22:03 -0500

    Added util methods

--- src/main/java/com/neoterux/tda/list/content/DoubleNode.java
+++ src/main/java/com/neoterux/tda/list/content/DoubleNode.java
@@ -40,6 +40,32 @@ public class DoubleNode<E> extends Node<E, DoubleNode<E>> {
         this.previous = previous;
     }
 
+    public void replaceWith(E element){
+        DoubleNode<E> nNode = new DoubleNode<>(element, this.previous, this.next);
+        if(this.next != null)
+            this.next.setPrevious(nNode);
+        if(this.previous != null)
+            this.previous.setNext(nNode);
+        this.clean();
+    }
+
+    public void delete() {
+        if(this.previous == this || this.next == this) {
+            clean();
+            return;
+        }
+        if(this.previous != null && this.next != null){
+            this.previous.setNext(this.next);
+            this.next.setPrevious(this.previous);
+        }
+
+        if(previous == null)
+            this.next.setPrevious(null);
+        else if(next == null)
+            this.previous.setNext(null);
+        // if any of the conditions are true, that means that this node is alone
+    }
+
     @Override
     public void clean() {
         this.next = null;
