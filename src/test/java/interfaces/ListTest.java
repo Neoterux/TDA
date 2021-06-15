8e8caa0ed00271dc110bd6ceea1f7897274e9844
neoterux <labfernandez2014@gmail.com>
2021-06-14 23:01:31 -0500

    Added ListTest template

---
+++ src/test/java/interfaces/ListTest.java
@@ -0,0 +1,32 @@
+package interfaces;
+
+public interface ListTest {
+
+    void addFirstTest();
+
+    void addLastTest();
+
+    void addTest();
+
+    void removeTest();
+
+    void removeFirstTest();
+
+    void removeLastTest();
+
+    void getTest();
+
+    void setTest();
+
+    void sizeTest();
+
+    void isEmptyTest();
+
+    void findAllTestEquals();
+
+    void findAllTestComparator();
+
+    void intersectionTest();
+
+    void clear();
+}
